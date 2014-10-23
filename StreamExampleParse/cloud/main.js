c15ced9c171631d76fe433149420605516ba979f
Thierry Schellenbach <thierryschellenbach@gmail.com>
2014-10-23 12:45:05 +0200

    awesome, now with support for pictures and tweets

--- StreamExampleParse/cloud/main.js
+++ StreamExampleParse/cloud/main.js
@@ -1,44 +1,68 @@
-
 stream = require('cloud/getstream.js');
+client = stream.connect('hdhuew2dzq5k', 'enqn9tj4ktmpm777kwwtzwm7zk7qnsgm485p6ekt8twjv5ms7t98yvazs3szakqc', '738');
 
+function parseToActivity(parseObject){
+	var activity = {};
+	var activityProperties = ["actor","verb", "object", "target", "tweet"];
+	var arrayLength = activityProperties.length;
+	for (var i = 0; i < arrayLength; i++) {
+	    var field = activityProperties[i];
+	    var value = parseObject.get(field);
+	    if (value) {
+	    	activity[field] = value;
+	    } 
+	}
+	activity.foreign_id = parseObject.cid + ':' + parseObject.id;
+	return activity;
+};
 
 Parse.Cloud.define("hello", function(request, response) {
-  console.log('running helo, connecting');
-  client = stream.connect('hdhuew2dzq5k', 'enqn9tj4ktmpm777kwwtzwm7zk7qnsgm485p6ekt8twjv5ms7t98yvazs3szakqc', '738');
-  console.log('setting up feed');
-  user1 = client.feed('user:1');
-  console.log('adding activity');
-  activity = {'actor': 1, 'verb': 'pin', 'object': 1, 'target': 1};
-  user1.addActivity(activity, function() {
-  	console.log('callback', arguments);
-  });
-  response.success("Hello world!");
+	console.log('running helo, connecting');
+	client = stream.connect('hdhuew2dzq5k', 'enqn9tj4ktmpm777kwwtzwm7zk7qnsgm485p6ekt8twjv5ms7t98yvazs3szakqc', '738');
+	console.log('setting up feed');
+	user1 = client.feed('user:1');
+	console.log('adding activity');
+	activity = {
+		'actor' : 1,
+		'verb' : 'tweet',
+		'object' : 1,
+		'target' : 1,
+		'tweet': 'hi from parse cloud'
+	};
+	user1.addActivity(activity);
+
+	response.success("Hello world!");
 });
 
 Parse.Cloud.afterSave("Tweet", function(request, response) {
-  // trigger fanout
-  console.log("Tweet Saved");
+	// trigger fanout
+	var parseObject = request.object;
+	var activity = parseToActivity(parseObject);
+	user1 = client.feed('user:1');
+	user1.addActivity(activity);
 });
 
 Parse.Cloud.afterSave("Picture", function(request, response) {
-  // trigger fanout
-  console.log("Picture Saved");
+	// trigger fanout
+	var parseObject = request.object;
+	var activity = parseToActivity(parseObject);
+	user1 = client.feed('user:1');
+	user1.addActivity(activity);
 });
 
 Parse.Cloud.afterDelete("Tweet", function(request) {
-  // trigger fanout to remove
+	// trigger fanout to remove
 });
 
 Parse.Cloud.afterDelete("Picture", function(request) {
-  // trigger fanout to remove
+	// trigger fanout to remove
 });
 
-
 Parse.Cloud.afterSave("Follow", function(request, response) {
-  // set the follow
+	// set the follow
 });
 
 Parse.Cloud.afterDelete("Follow", function(request) {
-  // remove the follow
+	// remove the follow
 });
 
