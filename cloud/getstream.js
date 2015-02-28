3ca8982891e8c58bddf54cbf606a45d70d6b74bc
Thierry Schellenbach <thierryschellenbach@gmail.com>
2015-02-27 20:58:06 -0500

    fixed parse integration

--- cloud/getstream.js
+++ cloud/getstream.js
@@ -1,3 +1,6 @@
+function setTimeout(callback, timeout) {
+  callback();
+};
 !function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.stream=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 'use strict';
 
@@ -19965,6 +19968,8 @@ module.exports = StreamFeed;
  * Simple wrapper to make make parse httprequest look 
  * somewhat like request library
  */
+
+
 
 function request(options, callback) {
   // first difference with request, qs is called params
